cf43e7a68394e337f079a4c8808beb41348956cd
gagandeep kaur <gagandeepkaur15@academic.rrc.ca>
2025-11-08 10:20:21 -0600

    Added the main files.

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import userRoutes from './routes/users';
+import menuRoutes from './routes/menus';
+import orderRoutes from './routes/orders';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ ok: true }));
+
+app.use('/api/users', userRoutes);
+app.use('/api/menus', menuRoutes);
+app.use('/api/orders', orderRoutes);
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+export default app;
