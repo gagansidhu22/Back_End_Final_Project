1e5d7583b01015e646d3dcf3b91774b1e6d50715
gagandeep kaur <gagandeepkaur15@academic.rrc.ca>
2025-11-14 15:43:38 -0600

    created utils.

---
+++ src/utils/emailService.ts
@@ -0,0 +1,16 @@
+import { transporter } from "../../config/emailConfig";
+
+export const sendEmail = async (to: string, subject: string, html: string) => {
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent successfully!");
+  } catch (error) {
+    console.error("Email failed:", error);
+  }
+};
